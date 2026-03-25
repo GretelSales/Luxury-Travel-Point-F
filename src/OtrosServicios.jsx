bcef2e5855058a848f44b156a2e086c5424993d7
Nayhan Provedo Labrada <nayhanprovedolabrada@gmail.com>
2026-03-24 21:33:14 -0400

    adding banner image 7

--- src/OtrosServicios.jsx
+++ src/OtrosServicios.jsx
@@ -97,11 +97,12 @@ export default function OtrosServicios() {
     const fetchBanner = async () => {
       try {
         setLoadingBanner(true);
-
         const lang = i18n.language.startsWith("es") ? "es" : "en";
         const API_URL = import.meta.env.VITE_API_URL;
 
-        const url = await axios.get(`${API_URL}/api/banner?lang=${lang}`);
+        const url = await axios.get(
+          `https://luxury-travel-point-frontend.onrender.com/api/banner?lang=${lang}`,
+        );
         console.log("FETCHING:", url);
 
         const res = await axios.get(url);
