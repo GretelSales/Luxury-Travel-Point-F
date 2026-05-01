6c67f79b66d02b7f0319cc7a214e653ba4212959
Nayhan Provedo Labrada <nayhanprovedolabrada@gmail.com>
2026-04-30 20:50:09 -0400

    chance the url

--- src/apiServiceInterest.js
+++ src/apiServiceInterest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  "https://luxury-travel-point-frontend.onrender.com/api/service-interest";
+  "https://Luxury-Travel-Point-B.onrender.com/api/service-interest";
 
 export const sendServiceInterest = async (payload) => {
   const token = localStorage.getItem("ltp_token");
